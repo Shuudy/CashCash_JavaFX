50052ec71b87a86cbe12a46eeacc69b36c9e85a6
Shuudy <ancieux.jerome059@gmail.com>
2024-04-24 20:41:25 +0200

    Add getters in TypeMateriel class and add dateVente attribute and xmlMateirle method in Materiel class

--- src/main/java/com/cashcash/Materiel.java
+++ src/main/java/com/cashcash/Materiel.java
@@ -1,9 +1,27 @@
 package com.cashcash;
 
+import java.time.LocalDate;
+
 public class Materiel {
     private int numSerie;
+    private LocalDate dateVente;
     private double prixVente;
     private String emplacement;
 
     private TypeMateriel leType;
+
+    public String xmlMateriel(){
+        String chaineMat = "<materiel numSerie=\"" + numSerie +"\">\n";
+
+        chaineMat += "<type refInterne=\"" + leType.getReferenceInterne() + "\" libelle=\"" + leType.getLibelleTypeMateriel() + "\" />\n";
+        chaineMat += "<famille codeFamille=\"SE\" libelle=\"serveur\" />\n";
+        chaineMat += "<date_installation>" + dateVente.toString() + "</date_installation>\n";
+        chaineMat += "<prix_vente>" + prixVente + "</prix_vente>\n";
+        chaineMat += "<emplacement>" + emplacement + "</emplacement>\n";
+        chaineMat += "<nbJourAvantEcheance>94</nbJourAvantEcheance>\n";
+
+        chaineMat += "</materiel>";
+
+        return chaineMat;
+    }
 }
--- src/main/java/com/cashcash/TypeMateriel.java
+++ src/main/java/com/cashcash/TypeMateriel.java
@@ -2,4 +2,12 @@ package com.cashcash;
 
 public class TypeMateriel {
     private String referenceInterne, libelleTypeMateriel;
+
+    public String getReferenceInterne() {
+        return referenceInterne;
+    }
+
+    public String getLibelleTypeMateriel() {
+        return libelleTypeMateriel;
+    }
 }

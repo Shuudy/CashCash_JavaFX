3bbbec0f601838e2a94f13c14323d38fd124010a
Shuudy <ancieux.jerome059@gmail.com>
2024-04-24 21:14:20 +0200

    Edit Materiel constructor

--- src/main/java/com/cashcash/Materiel.java
+++ src/main/java/com/cashcash/Materiel.java
@@ -6,16 +6,17 @@ import java.time.LocalDate;
 public class Materiel {
     private int numSerie;
     private LocalDate dateVente;
+    private LocalDate dateInstallation;
     private double prixVente;
     private String emplacement;
 
     private TypeMateriel leType;
 
-    public Materiel(int numSerie, String dateVente, double prixVente, String emplacement, TypeMateriel leType){
+    public Materiel(int numSerie, Date dateVente, Date dateInstallation, double prixVente, String emplacement, TypeMateriel leType){
         this.numSerie = numSerie;
 
-        Date sqlDate = Date.valueOf(dateVente);
-        this.dateVente = sqlDate.toLocalDate();
+        this.dateVente = dateVente.toLocalDate();
+        this.dateInstallation = dateInstallation.toLocalDate();
 
         this.prixVente = prixVente;
         this.emplacement = emplacement;
@@ -28,7 +29,7 @@ public class Materiel {
 
         chaineMat += "<type refInterne=\"" + leType.getReferenceInterne() + "\" libelle=\"" + leType.getLibelleTypeMateriel() + "\" />\n";
         chaineMat += "<famille codeFamille=\"SE\" libelle=\"serveur\" />\n";
-        chaineMat += "<date_installation>" + dateVente.toString() + "</date_installation>\n";
+        chaineMat += "<date_installation>" + dateInstallation.toString() + "</date_installation>\n";
         chaineMat += "<prix_vente>" + prixVente + "</prix_vente>\n";
         chaineMat += "<emplacement>" + emplacement + "</emplacement>\n";
         chaineMat += "<nbJourAvantEcheance>94</nbJourAvantEcheance>\n";

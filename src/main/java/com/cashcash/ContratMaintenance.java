1153a2d65fb0534039ec88f529a102605296a64a
Shuudy <ancieux.jerome059@gmail.com>
2024-04-24 22:02:58 +0200

    Add ContratMaintenance class

---
+++ src/main/java/com/cashcash/ContratMaintenance.java
@@ -0,0 +1,36 @@
+package com.cashcash;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.time.temporal.ChronoUnit;
+
+public class ContratMaintenance {
+    private int numContrat;
+    private LocalDate dateSignature, dateEcheance;
+    private ArrayList<Materiel> lesMaterielsAssures;
+
+    public ContratMaintenance(int numContrat, Date dateSignature, Date dateEcheance) {
+        this.numContrat = numContrat;
+        this.dateSignature = dateSignature.toLocalDate();
+        this.dateEcheance = dateEcheance.toLocalDate();
+        this.lesMaterielsAssures = new ArrayList<Materiel>();
+    }
+
+    public int getJoursRestants() {
+        LocalDate now = LocalDate.now();
+        long joursRestants = dateEcheance.until(now, ChronoUnit.DAYS);
+        return Math.toIntExact(joursRestants);
+    }
+
+    public boolean estValide() {
+        LocalDate now = LocalDate.now();
+        return now.isAfter(dateSignature) && now.isBefore(dateEcheance);
+    }
+
+    public void ajouteMateriel(Materiel unMateriel) {
+        if(dateSignature.isBefore(unMateriel.getDateInstallation())) {
+            lesMaterielsAssures.add(unMateriel);
+        }
+    }
+}

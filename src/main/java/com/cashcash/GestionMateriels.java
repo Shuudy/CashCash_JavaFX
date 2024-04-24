85e3e5ad65629d909fd54b32dff67b2198eda76e
Shuudy <ancieux.jerome059@gmail.com>
2024-04-24 23:30:44 +0200

    Add contrat maintenance to client when getClient

--- src/main/java/com/cashcash/GestionMateriels.java
+++ src/main/java/com/cashcash/GestionMateriels.java
@@ -22,9 +22,11 @@ public class GestionMateriels {
             ResultSet rs = ps.executeQuery();
 
             while(rs.next()) {
+                int clientNum = rs.getInt("id");
+
                 // On parcours les matériels du client que l'on parcours
                 PreparedStatement ps1 = conn.prepareStatement("SELECT m.id, m.saleDate, m.installationDate, m.salePrice, m.location, mt.internalRef, mt.label FROM materials m, materialstypes mt WHERE mt.internalRef=m.internalRef AND m.clientNum = ?");
-                ps1.setInt(1, rs.getInt("id"));
+                ps1.setInt(1, clientNum);
                 ResultSet rs1 = ps1.executeQuery();
 
                 ArrayList<Materiel> lesMateriels = new ArrayList<Materiel>();
@@ -32,6 +34,16 @@ public class GestionMateriels {
                     TypeMateriel tm = new TypeMateriel(rs1.getString("internalRef"), rs1.getString("label"));
                     Materiel m = new Materiel(rs1.getInt("id"), rs1.getDate("saleDate"), rs1.getDate("installationDate"), rs1.getDouble("salePrice"), rs1.getString("location"), tm);
                     lesMateriels.add(m);
+                }
+
+                // Contrat de maintenance
+                PreparedStatement ps2 = conn.prepareStatement("SELECT id, signatureDate, dueDate FROM maintenancecontracts WHERE clientNum = ?");
+                ps2.setInt(1, clientNum);
+                ResultSet rs2 = ps2.executeQuery();
+
+                ContratMaintenance cm = null;
+                while(rs2.next()) {
+                    cm = new ContratMaintenance(rs2.getInt("id"), rs2.getDate("signatureDate"), rs2.getDate("dueDate"));
                 }
 
                 Client unClient = new Client(
@@ -44,7 +56,8 @@ public class GestionMateriels {
                     rs.getString("mailAddress"),
                     rs.getInt("travelTime"),
                     rs.getInt("distanceKm"),
-                    lesMateriels
+                    lesMateriels,
+                    cm
                 );
 
                 return unClient;
@@ -53,5 +66,17 @@ public class GestionMateriels {
 			e.printStackTrace();	
 		}
         return null;
+    }
+
+    public String xmlClient(Client unClient) {
+        Connection conn = dc.getConnection();
+
+        try {
+
+        } catch (Exception e) {
+			e.printStackTrace();	
+		}
+
+        new ContratMaintenance(0, null, null)
     }
 }

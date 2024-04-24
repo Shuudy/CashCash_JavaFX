d99315404f324af49f7aef247beccc8006ed4f57
Shuudy <ancieux.jerome059@gmail.com>
2024-04-24 20:57:33 +0200

    Add constructor to TypeMateriel class

--- src/main/java/com/cashcash/TypeMateriel.java
+++ src/main/java/com/cashcash/TypeMateriel.java
@@ -3,6 +3,11 @@ package com.cashcash;
 public class TypeMateriel {
     private String referenceInterne, libelleTypeMateriel;
 
+    public TypeMateriel(String referenceInterne, String libelleTypeMateriel){
+        this.referenceInterne = referenceInterne;
+        this.libelleTypeMateriel = libelleTypeMateriel;
+    }
+
     public String getReferenceInterne() {
         return referenceInterne;
     }

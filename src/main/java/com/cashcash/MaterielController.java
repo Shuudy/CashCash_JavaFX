9e341af12f734c3163049fa70513ec1d78405fb8
Shuudy <ancieux.jerome059@gmail.com>
2024-04-26 20:32:30 +0200

    Add label, handleButtonClick on create contrat de maintenance

--- src/main/java/com/cashcash/MaterielController.java
+++ src/main/java/com/cashcash/MaterielController.java
@@ -1,11 +1,16 @@
 package com.cashcash;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -19,30 +24,52 @@ public class MaterielController implements Initializable {
     private TableColumn<Materiel, String> col_label;
 
     @FXML
+    private TableColumn<Materiel, String> col_emplacement;
+
+    @FXML
+    private TableColumn<Materiel, LocalDate> col_installationdate;
+
+    @FXML
+    private Label label_title;
+
+    @FXML
+    private Button button_create;
+
+    @FXML
     private TableView<Materiel> table_materiels;
 
     private Client selectedClient;
     
     public void setSelectedClient(Client selectedClient) {
         this.selectedClient = selectedClient;
+
+        loadMaterielData();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {        
         
         col_id.setCellValueFactory(new PropertyValueFactory<Materiel, Integer>("numSerie"));
-        //col_label.setCellValueFactory(new PropertyValueFactory<Materiel, String>("emplacement"));
+        col_label.setCellValueFactory(new PropertyValueFactory<Materiel, String>("libelleTypeMateriel"));
+        col_emplacement.setCellValueFactory(new PropertyValueFactory<Materiel, String>("emplacement"));
+        col_installationdate.setCellValueFactory(new PropertyValueFactory<Materiel, LocalDate>("dateInstallation"));
+    }
 
-        loadMaterielData();
+    @FXML
+    public void handleButtonClick() {
+        Alert a = new Alert(AlertType.INFORMATION);
+        a.setHeaderText(null);
+        a.setContentText("Contrat de maintenance créé avec succès pour les matériels sélectionné !");
+        a.show();
     }
 
     private void loadMaterielData() {
         if (selectedClient != null) {
-            System.out.println("good");
             ObservableList<Materiel> materielList = new DatabaseConnection().getMaterielForClient(selectedClient.getId());
             table_materiels.setItems(materielList);
+
+            label_title.setText("Matériels du client " + selectedClient.getRaisonSociale());
         } else {
-            // Handle the case where selectedClient is null (optional)
             System.out.println("selectedClient is null");
         }
     }

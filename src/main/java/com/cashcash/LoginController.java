eb2e8c33bf39e1a5074702e297df4f08d742d998
Shuudy <ancieux.jerome059@gmail.com>
2024-05-01 00:25:36 +0200

    Add login logical in handleButtonAuth class

--- src/main/java/com/cashcash/LoginController.java
+++ src/main/java/com/cashcash/LoginController.java
@@ -1,6 +1,12 @@
 package com.cashcash;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import javafx.fxml.FXML;
 
@@ -25,20 +31,29 @@ public class LoginController {
     private Label label_msg;
 
     @FXML
-    void handleButtonAuth(MouseEvent event) throws IOException {
+    void handleButtonAuth(MouseEvent event) throws IOException, SQLException {
 
         String email = txtfield_email.getText();
         String password = txtfield_password.getText();
-        
+
         if (!email.isEmpty() && !password.isEmpty()) {
-            if (email.equals("test") && password.equals("test")) {
-                App.setRoot("primary");
+            Connection cn = new DatabaseConnection().getConnection();
+            PreparedStatement ps = cn.prepareStatement("SELECT password FROM employees WHERE mailAddress = ?");
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                String passwordFromDB = rs.getString("password");
+                if (BCrypt.checkpw(password, passwordFromDB)) {
+                    App.setRoot("primary");
+                } else {
+                    label_msg.setText("L'adresse e-mail ou le mot de passe sont incorrect.");
+                }
             } else {
                 label_msg.setText("L'adresse e-mail ou le mot de passe sont incorrect.");
             }
         } else {
             label_msg.setText("Les champs doivent être remplis.");
         }
-
     }
 }

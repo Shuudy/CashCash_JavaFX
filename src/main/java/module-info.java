82f4dfd9ff827efa4095c29f600eac716e8b81cc
Shuudy <ancieux.jerome059@gmail.com>
2024-05-01 00:24:06 +0200

    Edit module-info.java with requires spring.security.crypto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.cashcash {
     requires javafx.fxml;
     requires java.sql;
     requires transitive javafx.graphics;
+    requires spring.security.crypto;
 
     opens com.cashcash to javafx.fxml;
     exports com.cashcash;

2b0fbb53a8d50a7bbd66609e0e44e7cf93f41d12
Shuudy <ancieux.jerome059@gmail.com>
2024-05-02 20:20:20 +0200

    Add famille to the TypeMateriel class

--- src/main/java/com/cashcash/TypeMateriel.java
+++ src/main/java/com/cashcash/TypeMateriel.java
@@ -2,11 +2,13 @@ package com.cashcash;
 
 /**
  * Représente un type de matériel.
- * Cette classe stocke les informations relatives à un type de matériel, telles que sa référence interne
- * et son libellé.
+ * Cette classe stocke les informations relatives à un type de matériel, telles que sa référence interne,
+ * son libellé et sa famille.
  */
 public class TypeMateriel {
     private String referenceInterne, libelleTypeMateriel;
+
+    private Famille laFamille;
 
     /**
      * Constructeur de la classe TypeMateriel.
@@ -14,9 +16,11 @@ public class TypeMateriel {
      * @param referenceInterne     La référence interne du type de matériel.
      * @param libelleTypeMateriel  Le libellé du type de matériel.
      */
-    public TypeMateriel(String referenceInterne, String libelleTypeMateriel){
+    public TypeMateriel(String referenceInterne, String libelleTypeMateriel, Famille laFamille){
         this.referenceInterne = referenceInterne;
         this.libelleTypeMateriel = libelleTypeMateriel;
+
+        this.laFamille = laFamille;
     }
 
     /**
@@ -35,5 +39,14 @@ public class TypeMateriel {
      */
     public String getLibelleTypeMateriel() {
         return libelleTypeMateriel;
+    }
+
+    /**
+     * Obtient la famille du type de matériel.
+     *
+     * @return La famille du type de matériel.
+     */
+    public Famille getLaFamille() {
+        return laFamille;
     }
 }
